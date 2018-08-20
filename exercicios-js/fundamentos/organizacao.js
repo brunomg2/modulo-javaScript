5ceb46253520f2bcc8486ff297ed89fdfcdbf8f0
Bruno Gabriel <brunomg2016@hotmail.com>
2018-08-20 09:23:28 -0300

    organização javaScrit

---
+++ exercicios-js/fundamentos/organizacao.js
@@ -0,0 +1,16 @@
+//Sentença de código
+console.log("Sentença de código")
+
+//Blocos de código
+{
+    let numero = 1
+    console.log(numero)
+}
+
+//Pode se ter blocos aninhados
+{
+    {
+        let numero = 2
+        console.log(numero)
+    }
+}
