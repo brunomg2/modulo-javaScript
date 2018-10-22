a63734cbabdd7ad15b10c94e3c2b2853fa413d4e
Bruno Nogueira <brunomg2016@hotmail.com>
2018-10-21 22:59:12 -0300

    entrada e saida padrão do node

--- exercicios-js/node/entradaESaida.js
+++ exercicios-js/node/entradaESaida.js
@@ -1,7 +1,7 @@
 const anonimo = process.argv.indexOf('-a') !== -1
 
 if(anonimo) {
-    process.stdout.write('Fala anônimo!!\n')
+    process.stdout.write('Fala anônimo!!!\n')
     process.exit()
 }else{
     process.stdout.write('Informe seu nome: ')
