d5a8b12357dded952929fef6bc8b9694300bae8a
Bruno Gabriel <brunomg2016@hotmail.com>
2018-10-23 10:43:33 -0300

    rest e spread, tagged Templates

---
+++ exercicios-js/esnext/operadorRestSpread.js
@@ -0,0 +1,11 @@
+//rest(juntar) spread(espalhar)
+
+//spread com objeto
+const funcionario = { nome: 'Maria', salario: 12348.99 }
+const clone = { ativo: true, ...funcionario }
+console.log(clone)
+
+//spread com array
+const grupoA = ['João', 'Pedro', 'Gloria']
+const grupoFinal = ['Maria', ...grupoA, 'Rafaela']
+console.log(grupoFinal)
