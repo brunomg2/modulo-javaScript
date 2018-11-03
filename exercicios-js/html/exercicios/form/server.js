a635c08e0abc10d0e2542320d5da65ef588ad0cf
Bruno Nogueira <brunomg2016@hotmail.com>
2018-11-03 20:53:22 -0300

    fim dados formulário

--- exercicios-js/html/exercicios/form/server.js
+++ exercicios-js/html/exercicios/form/server.js
@@ -5,8 +5,21 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.post('/usuarios', (req, resp) => {
-    console.log(req.body)
-    resp.send('<h1>Parabéns</h1>')
+    console.log(req.body) 
+    resp.send('<h1>Parabéns dados incluidos!</h1>')
 })
+
+app.post('/usuarios/:id', (req, resp) => {
+    console.log(req.params.id)
+    console.log(req.body)
+    resp.send('<h1>Parabéns dados alterados com sucesso!!!</h1>')
+})
+
+/*
+app.get('/usuarios', (req, resp) => {
+    console.log(req.query) se for do tipo get os dados vem atraves do req.query
+    resp.send('<h1>Parabéns dados incluidos!</h1>')
+})*/
+
 
 app.listen(3003)
